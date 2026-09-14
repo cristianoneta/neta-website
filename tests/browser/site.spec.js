907879e2d47596512b86ae2c871a57080f99e846
cristianoneta <chris_fms@mailbox.org>
2026-09-14 21:18:02 +0200

    Restore Uni-7 access through STAVR (#74)
    * Use STAVR for Uni-7 access
    
    * Test STAVR Uni-7 endpoints

--- tests/browser/site.spec.js
+++ tests/browser/site.spec.js
@@ -95,6 +95,8 @@ test("NETA Socials can suggest the hidden Uni-7 chain", async ({page}) => {
   await expect(button).toHaveText("· UNI-7 ADDED");
   const chain = await page.evaluate(() => window.__suggestedChain);
   expect(chain.chainId).toBe("uni-7");
+  expect(chain.rpc).toBe("https://juno.rpc.t.stavr.tech");
+  expect(chain.rest).toBe("https://juno.api.t.stavr.tech");
   expect(chain.feeCurrencies[0].coinMinimalDenom).toBe("ujunox");
   expect(chain.bech32Config.bech32PrefixAccAddr).toBe("juno");
 });
